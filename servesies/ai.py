3f87ebae0411976d3909f8566c8a9b2068f0f64f
iliya-baranovsiy <mr.Fredi2017@mail.ru>
2025-04-24 14:52:41 +0300

    Update

--- servesies/ai.py
+++ servesies/ai.py
@@ -7,7 +7,7 @@ from servesies.file_work import str_read
 async def AiAnalyse():
     client = OpenAI(
         base_url="https://openrouter.ai/api/v1",
-        api_key="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",
+        api_key="",
     )
     completion = client.chat.completions.create(
         model="deepseek/deepseek-chat:free",
